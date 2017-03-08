f366b715ff920648f3b0e23d19c46eccefd355ac
Bruna Lima <bruna.lima@veinteractive.com>
2017-03-08 17:18:29 +0000

    clean up

--- src/tree.js
+++ src/tree.js
@@ -16,7 +16,7 @@ var vis = d3.select("#body").append("svg:svg")
   .append("svg:g")
   .attr("transform", `translate(${m[3]}, ${m[0]})`);
 
-d3.json("outputDependency.json", json => {
+d3.json("src/outputDependency.json", json => {
   root = json;
   root.x0 = h / 2;
   root.y0 = 0;
